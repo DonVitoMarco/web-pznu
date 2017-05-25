3a63a99dcb951ebd7a391226f439d2bb52a29a36
Marek Chrapek <mchrapek@omnis.pl>
2017-05-25 18:56:09 +0200

    board page

--- app/js/profile/profile.controller.js
+++ app/js/profile/profile.controller.js
@@ -2,21 +2,24 @@
     'use strict';
 
     angular.module('app')
-        .controller('ProfileController', ['$scope', '$q', 'profileService', 'sessionService', 'routeService', ProfileCtrl]);
+        .controller('ProfileController', ['$scope', '$q', '$sce', 'profileService', 'sessionService', 'routeService', ProfileCtrl]);
 
-    function ProfileCtrl($scope, $q, profileService, sessionService, routeService) {
+    function ProfileCtrl($scope, $q, $sce, profileService, sessionService, routeService) {
 
         var vm = this;
         vm.username = '';
         vm.projects = [];
         vm.project = {};
+        vm.filter = '';
+        vm.search = '';
         vm.showMore = showMore;
+        vm.filterProjects = filterProjects;
 
         init();
 
         function init() {
             vm.username = sessionService.getParam('username');
-            console.log(vm.username);
+            vm.filter = 'ALL';
 
             getAllProjects();
         }
@@ -24,7 +27,19 @@
         function getAllProjects() {
             profileService.getAllProjects()
                 .then(function (data) {
-                    vm.projects = data;
+                    return data;
+                })
+                .then(function (filter) {
+                    vm.projects = [];
+                    if (vm.filter !== 'ALL') {
+                        for (var i = 0; i < filter.length; i++) {
+                            if (filter[i].category === vm.filter) {
+                                vm.projects.push(filter[i]);
+                            }
+                        }
+                    } else {
+                        vm.projects = filter;
+                    }
                     return {};
                 })
                 .catch(function (data) {
@@ -35,6 +50,28 @@
         function showMore(name) {
             sessionService.setParam('project', name);
             routeService.goToState('panel.details');
+        }
+
+        $scope.shareHtml = $sce.trustAsHtml(
+            '<i class="fa fa-facebook-square fa-2x share" aria-hidden="true"></i> ' +
+            '<i class="fa fa-google-plus-square fa-2x share" aria-hidden="true"></i> '
+        );
+
+        $scope.notificationsHtml = $sce.trustAsHtml(
+            '<hr>' +
+            '<div class="notification-info">' +
+                '<i class="fa fa-commenting-o" aria-hidden="true"></i> Marek add new comments' +
+            '</div>' +
+            '<hr>' +
+            '<div class="notification-info">' +
+                '<i class="fa fa-plus-square-o" aria-hidden="true"></i> You add new project' +
+            '</div>' +
+            '<hr>'
+        );
+
+        function filterProjects(filter) {
+            vm.filter = filter;
+            getAllProjects();
         }
 
     }
